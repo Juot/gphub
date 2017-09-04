2d443ae53156b30ae7ee4bda0ad5fbe367117bb1
Juot <gongpeng9449@gmail.com>
2017-09-04 15:13:15 +0800

    first commit

---
+++ first.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import re
+
+def first(sth_input):
+    sth_input = input('what you want print :')
+    return(sth_input)
+
+if __name__ == '__main__':
+    print(first('sth_input'))
