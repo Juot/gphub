f83ba0b589b67f37b982372efb1f2c0b14f8b27b
Juot <30500243+Juot@users.noreply.github.com>
2017-09-04 17:38:29 +0800

    Update scrapetest.py

--- scrapetest.py
+++ scrapetest.py
@@ -1,3 +1,5 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 html = urlopen("http://pythonscraping.com/pages/page1.html")
-print(html.read())
+bs0bj = BeautifulSoup(html.read())
+print(bs0bj.h1)
