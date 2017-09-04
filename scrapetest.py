0a639d684304ac8e747aca464861d1794c8814d7
Juot <gongpeng9449@gmail.com>
2017-09-04 21:20:24 +0800

    增加lxml

--- scrapetest.py
+++ scrapetest.py
@@ -1,5 +1,5 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 html = urlopen("http://pythonscraping.com/pages/page1.html")
-bs0bj = BeautifulSoup(html.read())
+bs0bj = BeautifulSoup(html.read(),'lxml')
 print(bs0bj.h1)
