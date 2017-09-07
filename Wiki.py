982a4fcdf798092574847cefe9dbdced114445ff
Juot <gongpeng9449@gmail.com>
2017-09-08 03:35:02 +0800

    2017年09月08日03:35:00

--- Wiki.py
+++ Wiki.py
@@ -4,6 +4,6 @@ import re
 
 html = urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
 bs0bj = BeautifulSoup(html,"lxml")
-for link in bs0bj.findAll("a"):
+for link in bs0bj.find("div",{"id":"bodyContent"}).findAll("a",href = re.compile("^(/wiki/)((?!:).)*$")):
     if 'href' in link.attrs:
         print(link.attrs['href'])
---
+++ getlinks.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import datetime
+import random
+
+random.seed(datetime.datetime.now())
+def getLinks(articleUrl):
+    html = urlopen("http://en.wikipedia.org"+articleUrl)
+    bs0bj = BeautifulSoup(html,"lxml")
+    return bs0bj.find("div",{"id":"bodyContent"}).findAll("a",href=re.compile("^(/wiki/)((?!:).)*$"))
+links = getLinks("/wiki/Kevin_Bacon")
+while len(links) > 0:
+    print(len(links),end = "  ")
+    ran = random.randint(0,len(links)-1)
+    print(ran)
+    print("*********",random.randint(0,len(links)-1),"***********")
+    newArticle = links[1].attrs["href"]
+    print(newArticle)
+    links = getLinks(newArticle)
