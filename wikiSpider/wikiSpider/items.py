25ad172eed96c0e96458d8c661b0f2e96864453f
Juot <gongpeng9449@gmail.com>
2017-09-13 02:08:17 +0800

    2017年09月13日02:08:14

--- wikiSpider/wikiSpider/items.py
+++ wikiSpider/wikiSpider/items.py
@@ -5,10 +5,10 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item,Field
 
 
-class WikispiderItem(scrapy.Item):
+class Article(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = Field()
